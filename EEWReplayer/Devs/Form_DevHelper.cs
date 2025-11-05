eba48948d46c7c01645e37b40283697302741012
Ichihai1415 <projects31415.gif@gmail.com>
2025-11-05 13:14:49 +0900

    構造調整

---
+++ EEWReplayer/Devs/Form_DevHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EEWReplayer.Devs
+{
+    public partial class Form_DevHelper : Form
+    {
+        public Form_DevHelper()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_DevHelper_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
